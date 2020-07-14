751603b4984e0a3433764a6b0ea74063a19aa658
Carlos Enrique Cimino <ciminoce@yahoo.com.ar>
2020-07-14 20:05:10 -0300

    Ejercicio completo

--- Ejercicios07Punto01/Program.cs
+++ Ejercicios07Punto01/Program.cs
@@ -8,6 +8,7 @@ namespace Ejercicios07Punto01
         static void Main(string[] args)
         {
             double[] tempSemana = new[] { 14.5, 10.7, 8.7, 10, 12, 14, 18 };//se define e inicializa el vector
+            //double[]tempSemana=new double[7];
             do
             {
                 #region Menu Principal
@@ -68,6 +69,18 @@ namespace Ejercicios07Punto01
                         opcionElegida = "Estadísticas";
                         Estadisticas(opcionElegida, tempSemana);
                         break;
+                    case 6://Listado Estadístico
+                        opcionElegida = "Listado Estadístico";
+                        ListaEstadistico(opcionElegida, tempSemana);
+                        break;
+                    case 7://Ordenar Vector
+                        opcionElegida = "Ordenar Vector";
+                        OrdenarVector(opcionElegida,tempSemana);
+                        break;
+                    case 8://Reiniciar Vector
+                        opcionElegida = "Reinicio";
+                        ReinicioDelVector(opcionElegida, tempSemana);
+                        break;
                     default:
                         Console.WriteLine("Opción no válida");
                         break;
@@ -79,10 +92,75 @@ namespace Ejercicios07Punto01
 
         }
 
+        private static void ReinicioDelVector(string opcionElegida, double[] tempSemana)
+        {
+            IngresoAlMetodo(opcionElegida);
+            string respuesta;
+            do
+            {
+                Console.Write("¿Desea borrar todos los datos del vector?(S/N):");
+                respuesta = Console.ReadLine();
+                if (respuesta.ToUpper()!="S" && respuesta.ToUpper()!="N")
+                {
+                    Console.WriteLine("Debe ingresar S o N... pruebe otra vez");
+                }
+                else
+                {
+                    break;
+                }
+
+            } while (true);
+
+            if (respuesta.ToUpper()=="S")
+            {
+                 Array.Clear(tempSemana,0,tempSemana.Length);
+            }
+        }
+
+        private static void OrdenarVector(string opcionElegida, double[] tempSemana)
+        {
+            IngresoAlMetodo(opcionElegida);
+            OrdenamientoDelVector(tempSemana);
+            MostrarDatosEnTabla(tempSemana,false);
+            SalidaDelMetodo(opcionElegida);
+        }
+
+        private static void OrdenamientoDelVector(double[] tempSemana)
+        {
+            /*ciclo para ir moviendo la posicion que se ordena
+             siempre arranca de la primera posicion esto es 0 y 
+            llega hasta la anteúltima */
+            for (int posicionOrdenar = 0; posicionOrdenar < tempSemana.Length - 1; posicionOrdenar++)
+            {
+                /*cilco para ir moviendo la posicion contra la cual voy comparando
+                 siempre arranca desde la posicion que estoy ordenando +1
+                y llega hasta el final del vector */
+                for (int posicionComparar = posicionOrdenar + 1; posicionComparar < tempSemana.Length; posicionComparar++)
+                {
+                    if (tempSemana[posicionOrdenar] > tempSemana[posicionComparar])
+                    {
+                        var auxiliar = tempSemana[posicionOrdenar]; //reservo el valor de la posicion que comparo
+                        tempSemana[posicionOrdenar] = tempSemana[posicionComparar];
+                        tempSemana[posicionComparar] = auxiliar;
+                    }
+                }
+            }
+        }
+
+        private static void ListaEstadistico(string opcionElegida, double[] tempSemana)
+        {
+            IngresoAlMetodo(opcionElegida);
+            double promedio = CalcularPromedio(tempSemana);
+            MostrarDatosEnTabla(tempSemana,promedio);
+            SalidaDelMetodo(opcionElegida);
+            
+
+        }
+
         private static void Estadisticas(string opcionElegida, double[] tempSemana)
         {
             IngresoAlMetodo(opcionElegida);
-            MostrarDatosEnTabla(tempSemana);
+            MostrarDatosEnTabla(tempSemana,false);
             var mayorTemperatura = HallarMayorTemperatura(tempSemana);
             var menorTemperatura = HallarMenorTemperatura(tempSemana);
             var promedioTemperaturas = CalcularPromedio(tempSemana);
@@ -135,7 +213,7 @@ namespace Ejercicios07Punto01
         private static void ModificarDatosPorContenido(string opcionElegida, double[] tempSemana)
         {
             IngresoAlMetodo(opcionElegida);
-            MostrarDatosEnTabla(tempSemana);
+            MostrarDatosEnTabla(tempSemana,false);
             Console.Write("Ingrese el valor a modificar:");
             var elementoModificar = double.Parse(Console.ReadLine());
             /*El metodo IndexOf me devuelve el indice del elemento a modificar
@@ -158,20 +236,59 @@ namespace Ejercicios07Punto01
         private static void ListarDatos(string opcionElegida, double[] tempSemana)
         {
             IngresoAlMetodo(opcionElegida);
-            MostrarDatosEnTabla(tempSemana);
+            MostrarDatosEnTabla(tempSemana,true);
             SalidaDelMetodo(opcionElegida);
         }
 
-        private static void MostrarDatosEnTabla(double[] tempSemana)
+        private static void MostrarDatosEnTabla(double[] tempSemana, double promedio)
         {
-            var tabla = new ConsoleTable("Día Nro", "Temperatura"); //Crea una tabla en consola
+            Console.Write($"El promedio de temperaturas es {promedio}");
+            ConsoleTable tabla=new ConsoleTable("Día Nro", "Temperatura","Mayor a Promedio");
+            for (int i = 0; i < tempSemana.Length; i++)
+            {
+                if (tempSemana[i]>promedio)
+                {
+                    tabla.AddRow(i,tempSemana[i],"*");
+                }
+                else
+                {
+                    tabla.AddRow(i, tempSemana[i],string.Empty);
+                }
+            }
+            tabla.Write();
+        }
+        private static void MostrarDatosEnTabla(double[] tempSemana,bool mostrarFahrenheit)
+        {
+            ConsoleTable tabla;
+            if (mostrarFahrenheit)
+            {
+                tabla = new ConsoleTable("Día Nro", "Temperatura","Fahrenheit"); //Crea una tabla en consola
+                
+            }
+            else
+            {
+                tabla = new ConsoleTable("Día Nro", "Temperatura");
+            }
             for (int i = 0; i < tempSemana.Length; i++)
             {
                 //Console.WriteLine($"{tempSemana[i]}");
-                tabla.AddRow(i + 1, tempSemana[i]); //Agrega una fila a la tabla en consola
+                if (mostrarFahrenheit)
+                {
+                    var fahrenheit = ConvertirFahrenheit(tempSemana[i]);
+                    tabla.AddRow(i + 1, tempSemana[i],fahrenheit); //Agrega una fila a la tabla en consola
+                }
+                else
+                {
+                    tabla.AddRow(i + 1, tempSemana[i]); //Agrega una fila a la tabla en consola
+                }
             }
 
             tabla.Write(); //Muestra la tabla en consola
+        }
+
+        private static double ConvertirFahrenheit(double celsius)
+        {
+            return celsius * 1.8 + 32;
         }
 
         private static void IngresoAlMetodo(string opcionElegida)
@@ -183,7 +300,7 @@ namespace Ejercicios07Punto01
         private static void ModificarDatos(string opcionElegida, double[] tempSemana)
         {
             IngresoAlMetodo(opcionElegida);
-            MostrarDatosEnTabla(tempSemana);
+            MostrarDatosEnTabla(tempSemana,false);
             var intIndex = 0;
             do
             {
@@ -212,7 +329,25 @@ namespace Ejercicios07Punto01
         private static void IngresarDatos(string opcionElegida, double[] tempSemana)
         {
             IngresoAlMetodo(opcionElegida);
-            //TODO:Desarrollar el ingreso de datos
+            for (int i = 0; i < tempSemana.Length; i++)
+            {
+                do
+                {
+                    Console.Write($"Ingrese la temperatura {i + 1}:");
+                    if (!double.TryParse(Console.ReadLine(),out tempSemana[i]))
+                    {
+                        Console.WriteLine("Temperatura mal ingresada");
+                    }else if (tempSemana[i]<-10 || tempSemana[i]>24)
+                    {
+                        Console.WriteLine("Temperatura fuera del rango permitido");
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                } while (true);
+            }
             SalidaDelMetodo(opcionElegida);
         }
 
